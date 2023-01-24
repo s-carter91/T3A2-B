bfc14a786851b86e302a37138c129f6964caae0c
s-carter91 <13502@coderacademy.edu.au>
2023-01-24 11:16:03 +1000

    schemas completed

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import gamesRoutes from './routes/games_routes'
+import gamesRoutes from './routes/games_routes.js'
 
 const app = express()
 
--- server/routes/games_routes.js
+++ server/routes/games_routes.js
@@ -1,8 +1,11 @@
-import { Express } from 'express'
+import express from 'express'
 import { GameModel } from '../db.js'
 
 const router = express.Router()
 
-router.get('/', async (req, res) => (await GameModel.find().populate({ path: 'platform', select: 'name'})))  // Will return all games with platform 
+router.get('/', async (req, res) => (await GameModel.find().
+    populate({ path: 'platform', select: 'name'}).
+    populate({ path: 'genre', select: 'name'})
+))  // Will return all games with platform and genre names
 
 export default router
