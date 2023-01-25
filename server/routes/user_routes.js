388932bf87965f7025fd24ddd548fc1b21edd19a
s-carter91 <13502@coderacademy.edu.au>
2023-01-25 15:34:53 +1000

    users routes

--- server/routes/user_routes.js
+++ server/routes/user_routes.js
@@ -3,14 +3,20 @@ import { RatingModel, ReviewModel, GameModel } from '../models/UserModel.js'
 
 const router = express.Router()
 
-router.put('/playing/', async (req, res) => {
+router.patch('/playing/', async (req, res) => {
     try {
         const { userId , gameId } = req.body
-        const gameObject = await GameModel.findOne({ gameId })
-        const userObject = await UserProfileModel.findOne({ userId })
+        const gameObject = await GameModel.findById({ _id : gameId })
+        const userObject = await UserProfileModel.findById({ _id : userId })
         if (gameObject) {
             if (userObject) {
-                userObject.currentGames.push(gameObject)
+                if (userObject.currentGames.includes(gameObject._id)) {
+                    res.status(400).send({ error: 'That game is already in your currently playing list' })
+                } else {
+                    userObject.currentGames.push(gameObject)
+                    await userObject.save()
+                    res.send(`${gameObject.name} added to your current games list`)
+                }
             } else {
                 res.status(404).send({ error: 'User not found' })
             }
@@ -18,22 +24,26 @@ router.put('/playing/', async (req, res) => {
             res.status(404).send({ error: 'Game not found' })
         }
     } catch (err){
-        res.status(500).send({ error: err.message})
+        res.status(500).send({ error: err.message })
     }
 })
 
 router.patch('/completed/', async (req, res) => {
     try {
         const { userId , gameId } = req.body
-        const gameObject = await GameModel.findByID({ gameId: gameId })
-        const userObject = await UserModel.findByID({ userId: userId })
+        const gameObject = await GameModel.findById({ _id : gameId })
+        const userObject = await UserProfileModel.findOne({ _id: userId })
         if (gameObject) {
             if (userObject) {
-                if (userObject.currentGames.indexof(gameObject) >= 0) {
-                    const index = userObject.currentGames.indexof(gameObject)
+                if (userObject.completedGames.includes(gameObject._id)) {
+                    res.status(400).send({ error: 'Game already in list'})                    
+                } else {
+                    const index = userObject.currentGames.indexOf(gameObject)
                     userObject.currentGames.splice(index, 1)
-                } else {}
-                userObject.completedGames.push(gameObject)
+                    userObject.completedGames.push(gameObject)
+                    userObject.save()
+                    res.send(`${gameObject.name} added to your completed games`)
+                }
             } else {
                 res.status(404).send({ error: 'User not found' })
             }
@@ -41,7 +51,7 @@ router.patch('/completed/', async (req, res) => {
             res.status(404).send({ error: 'Game not found' })
         }
     } catch (err){
-        res.status(500).send({ error: err.message})
+        res.status(500).send({ error: err.message })
     }
 })
 
