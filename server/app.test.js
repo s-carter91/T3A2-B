501e8a129486b022270c9a8c732de9421f52b307
Alvin <alvinz_lai@hotmail.com>
2023-01-29 11:12:14 +1000

    Test for games added

--- server/app.test.js
+++ server/app.test.js
@@ -9,10 +9,11 @@ describe('App tests', () => {
         expect(res.body.info).toBeDefined() // This line is good for checking if a key  exists. Seems redundant but will give more information to the developer of where it fails.
         expect(res.body.info).toBe('Backloggo')
     })
+    
     test('Add a game to user profile currently playing', async () => {
         const res = await request(app).patch('/users/playing').send({
-            gameId: '63d14625a9c87120c737c102',
-            userId: '63d14625a9c87120c737c106'
+            gameId: '63d142d3c1778b1046219bdf',
+            userId: '63d142d3c1778b1046219be3'
         })
         })
         expect(res.status).toBe(201)
@@ -23,3 +24,28 @@ describe('App tests', () => {
         // expect(res.body.category).toBe('Work')
         // expect(res.body.content).toBe('Just Testing G')
     })
+
+    // describe('Get games list', () => {
+    //     let res
+
+    //     beforeEach(async () => {
+    //         res = await request(app).get( '/games')
+    //         expect(res.status).toBe(200)
+    //         expect(res.headers['Content-Type']).toMatch('/json/i')
+    //     })
+
+    //     it('Has an element with the correct structure and data', () => {
+    //         res.body.forEach(el => {
+    //             expect(el._id).toBeDefined()
+    //             expect(el.name).toBeDefined()
+    //             expect(el.genre).toBeDefined()
+    //             expect(el.platform).toBeDefined()
+    //             expect(el._id.length).toBe(24)  
+    //             expect(el.genre.length).toBe(24)
+    //             expect(el.platform).toBe(24)
+    //         })
+    //         expect(res.body[0].name).toBe('God of War')
+    //     })
+
+    //     test('Create ')
+    
