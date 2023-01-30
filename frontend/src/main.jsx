2f3abbf6199ea8baa5f38454756d1d2f65876c8a
Alvin <alvinz_lai@hotmail.com>
2023-01-30 20:42:51 +1000

    Broswer router moved to main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,4 +6,8 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+<BrowserRouter>
+<App />
+</BrowserRouter>
+)
